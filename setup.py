083e637dcb4789dee3db4599c6f891a7a2050e46
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2022-05-05 13:33:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setuptools.setup(
       version='0.0.1',
       packages=setuptools.find_packages(),
       package_dir={'gym_surfacecode'},
+      package_data={'gym_surfacecode': ['envs/referee_decoder/X_decoder','envs/referee_decoder/DP_decoder']},
     classifiers=[
         "Programming Language :: Python :: 3",
     ]
