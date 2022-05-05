4632471ce7ea30544d011be9f9d3bbd64a250125
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2022-05-05 13:11:37 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name='gym_surfacecode',
       version='0.0.1',
       install_requires=['gym','keras'],
       packages=['gym_surfacecode'],
-      package_dir={'gym_surfacecode': 'mypkg'},
+      package_dir={'gym_surfacecode'},
       package_data={'gym_surfacecode': ['envs/referee_decoder/X_decoder','envs/referee_decoder/DP_decoder']},)
