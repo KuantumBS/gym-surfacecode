4480e80b501dba33a6fa5965a72e2a172a4390f2
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2022-05-05 13:27:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setuptools.setup(
       version='0.0.1',
       packages=setuptools.find_packages(),
       package_dir={'gym_surfacecode'},
-      install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3",
     ]
